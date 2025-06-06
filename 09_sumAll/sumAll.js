088377996c066549ea362b86d748dc3fc1819c21
Gregor <174007895+gloffler@users.noreply.github.com>
2025-06-06 11:12:31 +0200

    feat: exercise 09 max min value difference

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(first, second) {
+    if (isNaN(first) || isNaN(second)) return "Error";
 
+    const min = first < second ? first : second;
+    const max = first > second ? first : second;
+
+    let sum = min;
+    for (let i = min; i < max; i++) {
+        sum += i + 1;
+    }
+    return sum;
 };
 
 // Do not edit below this line
