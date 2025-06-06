8839dddcbe31ff568e348e440d3fda84d3284443
Gregor <174007895+gloffler@users.noreply.github.com>
2025-06-06 10:13:36 +0200

    Done Exercise 06

--- 06_repeatString/repeatString.js
+++ 06_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    let n = [];
+    for (let i = 0; i < num; i++) {
+        n.push(string);
+    }
+    return n.join("");
 };
 
 // Do not edit below this line
--- 07_reverseString/reverseString.js
+++ 07_reverseString/reverseString.js
@@ -1,5 +1,5 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    return [].push(string).split().reverse();
 };
 
 // Do not edit below this line
