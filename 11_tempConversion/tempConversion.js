f3ddb9be4f3289a7835bc64b4ac4d49bb910f93c
Gregor <174007895+gloffler@users.noreply.github.com>
2025-06-07 20:37:57 +0200

    fix: switch convertToFahrenheit and convertToCelsius

--- 11_tempConversion/tempConversion.js
+++ 11_tempConversion/tempConversion.js
@@ -1,9 +1,8 @@
 const convertToCelsius = function(input) {
-  
+  return (input - 32) / (5/9).toFixed(1);
 };
 
 const convertToFahrenheit = function(input) {
-  return (input - 32) / (5/9).toFixed(1);
 };
 
 // Do not edit below this line
