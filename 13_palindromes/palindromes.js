c7fe22bbb9eedaf73b2bb9f8f7484394694b7840
Gregor <174007895+gloffler@users.noreply.github.com>
2025-06-15 17:56:59 +0200

    Done Exercise 13

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,10 @@
-const palindromes = function () {
-
+const palindromes = function (phrase) {
+  const blacklist = ["!", " ", ".", ","];
+  phrase = phrase
+    .toLowerCase()
+    .split("")
+    .filter((a) => !blacklist.includes(a));
+  return phrase.toReversed().toString() === phrase.toString();
 };
 
 // Do not edit below this line
