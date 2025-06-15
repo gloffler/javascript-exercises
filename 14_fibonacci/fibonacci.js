80df7655f439ed6c273df4359772a55f9c14d9f9
Gregor <174007895+gloffler@users.noreply.github.com>
2025-06-15 18:52:36 +0200

    Done Exercise 14

--- 14_fibonacci/fibonacci.js
+++ 14_fibonacci/fibonacci.js
@@ -1,6 +1,21 @@
-const fibonacci = function() {
+const fibonacci = function(number) {
+  number = Number(number);
+  if (number < 0) return "OOPS";
+  let a = 0;
+  let b = 1;
+  let sum = 0;
 
+  for (let i = 0; i < number; i++) {
+    a = sum;
+    sum = a + b;
+    console.log(a + " + " + b + " = " + sum);
+    b = a;
+  }
+
+  return sum;
 };
 
 // Do not edit below this line
 module.exports = fibonacci;
+
+console.log(fibonacci(10));
