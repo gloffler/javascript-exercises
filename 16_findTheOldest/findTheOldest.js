6e10593dfbda6d17a7a4081802a5a9feb65342f9
Gregor Löffler <174007895+gloffler@users.noreply.github.com>
2025-06-19 18:04:06 +0200

    Done Exercise 16

--- 16_findTheOldest/findTheOldest.js
+++ 16_findTheOldest/findTheOldest.js
@@ -1,5 +1,12 @@
-const findTheOldest = function() {
+const findTheOldest = function (arr) {
+  return arr.toSorted((a, b) => {
+    const deathYearA = a.yearOfDeath || new Date().getFullYear();
+    const deathYearB = b.yearOfDeath || new Date().getFullYear();
+    const ageA = deathYearA - a.yearOfBirth;
+    const ageB = deathYearB - b.yearOfBirth;
 
+    return ageB - ageA;
+  })[0];
 };
 
 // Do not edit below this line
